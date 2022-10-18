bb5f83b26d2c07aa70ed34e4fe8dbee2819cbc5e
a8hok <admin@admins-MacBook-Pro.local>
2022-10-18 08:22:21 +0530

    removed morgan configuration from config

--- Config/config.js
+++ Config/config.js
@@ -1,10 +1,7 @@
-// import morgan from "morgan";
 import * as dotenv from 'dotenv';
 dotenv.config();
 
 const PORT = process.env.PORT || 8080;
-
-// const logger = morgan("combined");
 
 // const mongoUrl = `mongodb+srv://wildbadger:${process.env.PASSWORD}@clusterbankapp.pn7rge3.mongodb.net/?retryWrites=true&w=majority`;
 
